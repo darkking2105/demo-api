9cee898d15f46cf68f7c0bf39b38443edb432ff1
Maddy <sriramelumalai2002@gmail.com>
2026-04-18 11:35:36 +0530

    demo

---
+++ demo-api/src/test/java/com/Maddy/demo_api/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Maddy.demo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
