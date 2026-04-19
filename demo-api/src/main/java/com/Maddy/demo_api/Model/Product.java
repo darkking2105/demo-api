78d5f4c9fc34fb0dfe58cb930e3f0c0783735c48
Maddy <sriramelumalai2002@gmail.com>
2026-04-19 07:56:57 +0530

    feat: add Product entity and H2 config

---
+++ demo-api/src/main/java/com/Maddy/demo_api/Model/Product.java
@@ -0,0 +1,22 @@
+package com.Maddy.demo_api.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+@Entity
+@Data
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+
+}
